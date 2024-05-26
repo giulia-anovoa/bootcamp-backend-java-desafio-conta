6cce8d1c8b8ab9c03fe6de29900e8f7d1cc412bb
Giulia de Avellar Nóvoa <giulia.novoa@aluno.ufabc.edu.br>
2024-05-26 14:10:36 -0300

    Desafio conta bancária

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+        
+        System.out.println("Por favor, digite o número da Conta: ");
+        int conta = scanner.nextInt();
+        scanner.nextLine();
+        
+        System.out.println("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+        
+        System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+        
+        scanner.close();
     }
 }
